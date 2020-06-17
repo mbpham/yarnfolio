a54a65a4cbfbd5d41101bbe62535264e83e7bdd1
Maria Bich Pham <bntp.aabc@gmail.com>
2020-06-17 09:48:02 +0200

    cart reducer setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './pages/signin-signup/signin-signup.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -47,17 +47,19 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route path='/shop' component={ ShopPage } />
-          <Route path='/signin' component={ SignInSignUp } />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to=''/>) : (<SignInSignUp />)} />
         </Switch>
       </div>
     );
-
   }
-
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './cart-dropdown.styles.scss';
+import CustomButtom from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { connect } from 'react-redux';
+
+const Cart = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {
+        cartItems.map(cartItem => (<CartItem key={ cartItem.id } item={ cartItem }/>))
+      }
+    </div>
+    <CustomButtom> GO TO CHECKOUT </CustomButtom>
+  </div>
+)
+
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+
+export default connect(mapStateToProps, null)(Cart);
