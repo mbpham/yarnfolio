03163181574b28d6d486952fedc48a359eaccf0c
Maria Bich Pham <bntp.aabc@gmail.com>
2020-06-17 10:39:21 +0200

    added selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import './cart-dropdown.styles.scss';
 import CustomButtom from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const Cart = ({ cartItems }) => (
   <div className='cart-dropdown'>
@@ -16,8 +17,6 @@ const Cart = ({ cartItems }) => (
 )
 
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
-})
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state)})
 
 export default connect(mapStateToProps, null)(Cart);
